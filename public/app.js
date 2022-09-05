bbe3710fd72646344a267c4d06c4007fb42905f0
williampettersson <william.c.pettersson@gmail.com>
2022-09-05 12:46:26 +0200

    button working properly

--- public/app.js
+++ public/app.js
@@ -2,5 +2,5 @@ console.log("Hello from javascript");
 
 function myFunction() {
     var x = document.getElementById("myTextarea").value;
-    document.getElementById("demo").innerHTML = x;
+    document.getElementById("demo").innerHTML = "Hej, " + x;
 }
